b608ca007ca009a09d6294469c09690252c1d7f1
Gessielle <gessielle-barossi@hotmail.com>
2020-09-18 22:45:04 -0300

    Gritador

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Família;
+
+            Console.Write("Digite olá");
+            Família = Console.ReadLine();
+            Console.Write("\n");
+
+            Console.WriteLine($"{Família}!!!".ToUpper());
+
+            Console.WriteLine("Pressione Enter para sair");
+            Console.ReadKey();
+
+        }
+    }
+}
